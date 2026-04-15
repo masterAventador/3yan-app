f8f8e3c8d11a2d189c2efd2eae7523df59635fa6
Aventador <ws419460186@gmail.com>
2026-04-16 01:00:27 +0800

    refactor: 移除 shared_preferences 依赖
    - pubspec.yaml 去掉 shared_preferences
    - widget_test.dart 去掉 SharedPreferences.setMockInitialValues
    - 新增 ios/Podfile.lock

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sanyan_user/sanyan_user.dart';
 
 import 'package:sanyan_app/main.dart';
 
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
-    SharedPreferences.setMockInitialValues({});
     await LocalStorage.init();
     await tester.pumpWidget(const SanyanApp());
     // Flush the Future.delayed timer in SplashPage
